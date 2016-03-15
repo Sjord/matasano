a4aa9f4210c979335072a9e8c15b83e6995e0af2
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2016-03-15 16:32:02 +0100

    Implement CTR, the stream cipher mode

---
+++ set3/ctr.py
@@ -0,0 +1,24 @@
+from Crypto.Cipher import AES
+from struct import pack
+from base64 import b64decode
+
+ciphertext = 'L77na/nrFsKvynd6HzOoG7GHTLXsTVu9qvY/2syLXzhPweyyMTJULu/6/kXX0KSvoOLSFQ=='
+
+key='YELLOW SUBMARINE'
+nonce=0
+# format=64 bit unsigned little endian nonce,
+#        64 bit little endian block count (byte count / 16)
+
+def get_stream(nonce, key, length):
+    aes = AES.new(key, AES.MODE_ECB)
+    stream = b''
+    for i in range(0, 1 + (length // 16)):
+        counter = pack('<QQ', nonce, i)
+        hash = aes.encrypt(counter)
+        stream += hash
+    return stream[:length]
+
+ciphertext = b64decode(ciphertext)
+stream = get_stream(nonce, key, len(ciphertext))
+plain = bytearray([c ^ s for c, s in zip(ciphertext, stream)])
+print(plain)
