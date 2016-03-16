3affdd9e02f78081fd17ed7d19ea5b05ee217879
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2016-03-16 09:54:59 +0100

    Change mt19937 to 32-bits version
    As described we should use in the next excercise.

--- set3/mt19937_64.py
+++ set3/mt19937_64.py
@@ -1,13 +1,12 @@
-(w, n, m, r) = (32, 624, 397, 31)
-a = 0x9908B0DF
-(u, d) = (11, 0xFFFFFFFF)
-(s, b) = (7, 0x9D2C5680)
-(t, c) = (15, 0xEFC60000)
-l = 18
-f = 1812433253
-w_mask = 0xFFFFFFFF
+(w, n, m, r) = (64, 312, 156, 31)
+a = 0xB5026F5AA96619E9
+(u, d) = (29, 0x5555555555555555)
+(s, b) = (17, 0x71D67FFFEDA60000)
+(t, c) = (37, 0xFFF7EEE000000000)
+l = 43
+f = 6364136223846793005
+w_mask = 0xFFFFFFFFFFFFFFFF
 index = n
-
 
 def w_trunc(number):
     return number & w_mask
