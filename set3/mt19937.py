97ec27a9d5e8cfdb038fcd18711a18a219e2e424
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2016-03-25 13:53:35 +0100

    Make mt19937 a class
    So that we can have multiple instances with their own states.

--- set3/mt19937.py
+++ set3/mt19937.py
@@ -8,51 +8,56 @@ a = 0x9908B0DF
 l = 18
 f = 1812433253
 w_mask = 0xFFFFFFFF
-index = n
-
 
 def w_trunc(number):
     return number & w_mask
 
-x = []
-def init(seed):
-    global x, index
-    x = [seed]
-    for i in range(1, n):
-        x.append(w_trunc(f * (x[i - 1] ^ (x[i - 1] >> (w - 2))) + i))
-    index = n
 
+class Random:
+    def __init__(self, seed):
+        self.x = [seed]
+        for i in range(1, n):
+            self.x.append(w_trunc(f * (self.x[i - 1] ^ (self.x[i - 1] >> (w - 2))) + i))
+        self.index = n
 
-def next_int():
-    global index
+    def next_int(self):
+        if self.index >= n:
+            self.twist()
 
-    if index >= n:
-        twist()
+        y = self.x[self.index]
+        y = y ^ ((y >> u) & d)
+        y = y ^ ((y << s) & b)
+        y = y ^ ((y << t) & c)
+        y = y ^ (y >> l)
 
-    y = x[index]
-    y = y ^ ((y >> u) & d)
-    y = y ^ ((y << s) & b)
-    y = y ^ ((y << t) & c)
-    y = y ^ (y >> l)
+        self.index += 1
+        return w_trunc(y)
 
-    index += 1
-    return w_trunc(y)
-
-
-def twist():
-    global index
-
-    lower_mask = (1 << r) - 1
-    upper_mask = w_trunc(~lower_mask)
-    for i in range(n):
-        y = (x[i] & upper_mask) + (x[(i+1) % n] & lower_mask)
-        yA = y >> 1
-        if (y & 1):
-            yA = yA ^ a
-        x[i] = x[(i+m) % n] ^ yA
-    index = 0
+    def twist(self):
+        lower_mask = (1 << r) - 1
+        upper_mask = w_trunc(~lower_mask)
+        for i in range(n):
+            y = (self.x[i] & upper_mask) + (self.x[(i+1) % n] & lower_mask)
+            yA = y >> 1
+            if (y & 1):
+                yA = yA ^ a
+            self.x[i] = self.x[(i+m) % n] ^ yA
+        self.index = 0
 
 
 if __name__ == "__main__":
-    init(int(time.time()))
-    print(next_int())
+    random = Random(1)
+    for i in range(10):
+        print(random.next_int())
+
+# 1791095845
+# 4282876139
+# 3093770124
+# 4005303368
+# 491263
+# 550290313
+# 1298508491
+# 4290846341
+# 630311759
+# 1013994432
+        
