d6d4eeff0112b26c22957eab9a45ac7d4cd80d03
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2016-04-01 16:07:27 +0200

    Bleichenbacher's PKCS 1.5 Padding Oracle (Simple Case)

--- set6/rsableich.py
+++ set6/rsableich.py
@@ -1,117 +1,105 @@
-from base64 import b64decode 
-from math import floor, ceil
 
-e = 3
-n = 102475686917025296766570439272120624752510811998694234970181967099024303462967
-d = 68317124611350197844380292848080416501246442637454958102623396449974256861051 
+
+
+e = 3 
+n = 90587157434501026549952310384970631951370855552491309829786573238506360478351
+d = 60391438289667351033301540256647087967178072160209458029332904639264889982075
 bsize = 32
 
 msg = b'kick it, CC'
 
-def egcd(a, b):
-    if a == 0:
-        return (b, 0, 1)
-    else:
-        g, y, x = egcd(b % a, a)
-        return (g, x - (b // a) * y, y)
-
-def modinv(a, m):
-    g, x, y = egcd(a, m)
-    if g != 1:
-        raise Exception('modular inverse does not exist')
-    else:
-        return x % m
-
-def is_conforming(cipher_num):
+def is_padded(cipher_num):
     plain_num = pow(cipher_num, d, n)
     plain = plain_num.to_bytes(bsize, 'big')
     return plain.startswith(b'\0\2')
 
-def multiply(s, cipher_num):
-    return (s ** e * cipher_num) % n
 
-def pad(msg_num):
-    msg = msg_num.to_bytes(bsize, 'big')
-    msg = b'\0\2' + msg[2:]
-    return int.from_bytes(msg, 'big')
+padded = b'\0\2' + b'\0' * (bsize - 2 - len(msg)) + msg
+assert len(padded) == bsize
 
-def step2c(M, s):
-    # 2c
-    print(M)
-    a, b = M
-
-    r = ceil(2 * ((b * s - 2 * B) / n))
-    assert r >= 2*((b * s - 2 * B) / n)
-
-    while True:
-        low_s = ceil((2 * B + r * n) / b)
-        high_s = ceil((3 * B + r * n) / a)
-
-        for s_attempt in range(low_s, high_s):
-            assert s_attempt < (3 * B + r * n) / a
-            assert s_attempt >= (2 * B + r * n) / b
-
-            if is_conforming(multiply(s_attempt, cipher_num)):
-                return r, s_attempt
-        r += 1
+msg_num = int.from_bytes(padded, 'big')
+cipher_num = pow(msg_num, e, n)
+assert is_padded(cipher_num)
 
 
-def step3(M, s):
-    a, b = M
-    low_r = ceil((a * s - 3 * B + 1) / n)
-    high_r = floor((b * s - 2 * B) / n)
+B = pow(2, 8 * (bsize - 2))
 
-    assert low_r == high_r
+def divceil(a, b):
+    res = a // b
+    if a % b:
+        res += 1
+    return res
 
-    r = low_r
-    assert r >= (a * s - 3 * B + 1) / n
-    assert r <= (b * s - 2 * B) / n
-
-    a, b = (max(a, ceil((2 * B + r * n) / s)), min(b, floor((3 * B - 1 + r * n) / s)))
-
-    assert b >= a
-    return (a, b)
-
+def divfloor(a, b):
+    return a // b
 
 def step2a():
-    s = ceil(n / (3 * B))
-    assert s >= n / (3 * B)
-
+    s = divceil(n, (3*B))
     while True:
-        if is_conforming(multiply(s, cipher_num)):
+        if is_padded(cipher_num * s ** e % n):
             return s
+
         s += 1
 
 
-msg_num = int.from_bytes(msg, 'big')
-msg_num = pad(msg_num)
-cipher_num = pow(msg_num, e, n)
+def step2b(s):
+    while True:
+        s += 1   
+        if is_padded(cipher_num * s ** e % n):
+            return s
 
-assert is_conforming(cipher_num)
 
-B = 1 << (bsize * 8 - 16)
-M = (2*B, 3*B-1)
+def step2c(s, M):
+    assert len(M) == 1
+    (a, b), = M
 
-assert B == pow(2, 8 * (bsize - 2))
+    low_r = divceil(2 * b * s - 2 * B, n)
+    r = low_r
+    
+    while True:
+        low_s = divfloor(2 * B + r * n, b)
+        high_s = divceil(3 * B + r * n, a)
 
-# 2a
+        for s in range(low_s, high_s):
+            if is_padded(cipher_num * s ** e % n):
+                return s
+        
+        r += 1
+
+
+def step3(s, M):
+    new_M = set()
+
+    for a, b in M:
+        low_r = divceil(a * s - 3 * B + 1, n)
+        high_r = divfloor(b * s - 2 * B, n)
+
+        for r in range(low_r, 1 + high_r):
+            new_a = max(a, divceil(2 * B + r * n, s))
+            new_b = min(b, divfloor(3 * B - 1 + r * n, s))
+            new_M.add((new_a, new_b))
+
+    return new_M
+
+def has_solution(M):
+    if len(M) > 1:
+        return False
+    (a, b), = M
+    return a == b
+
+
+M = {(2 * B, 3 * B - 1)}
 s = step2a()
-assert s
-assert is_conforming(multiply(s, cipher_num))
+M = step3(s, M)
 
-M = step3(M, s)
+while not has_solution(M):
+    if len(M) > 1:
+        s = step2b(s)
+    else:
+        s = step2c(s, M)
 
-while M[0] < M[1]:
-    # 2c
-    r, s = step2c(M, s)
+    M = step3(s, M)
 
-    assert is_conforming(multiply(s, cipher_num))
-
-    assert s >= (2 * B + r * n) / M[1]
-    assert s < (3 * B + r * n) / M[0]
-
-    # 3
-    M = step3(M, s)
-
-plain_num = M[0]
-print(plain_num.to_bytes(bsize, 'big'))
+    if len(M) == 1:
+        (a, b), = M
+        print(b.to_bytes(bsize, 'big'))
