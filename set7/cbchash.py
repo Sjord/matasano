b43e2faf906420b4498cbd720cb6b4edf1e2ae58
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2016-04-04 11:44:57 +0200

    Hashing with CBC-MAC (padded)

--- set7/cbchash.py
+++ set7/cbchash.py
@@ -1,7 +1,11 @@
 from Crypto.Cipher import AES
 from binascii import hexlify
 
-def mac(key, plaintext):
+def mac(key, plaintext, padded=True):
+    if padded:
+        pad = 16 - len(plaintext) % 16
+        plaintext += bytes([pad]) * pad
+
     iv = b'\0' * 16 
     aes = AES.new(key, AES.MODE_CBC, IV=iv)
     ciphertext = aes.encrypt(plaintext)
@@ -12,6 +16,13 @@ def ecb(key, plaintext):
     assert len(plaintext) == 16
     aes = AES.new(key, AES.MODE_ECB)
     ciphertext = aes.encrypt(plaintext)
+    return ciphertext
+
+
+def ecb_decrypt(key, plaintext):
+    assert len(plaintext) == 16
+    aes = AES.new(key, AES.MODE_ECB)
+    ciphertext = aes.decrypt(plaintext)
     return ciphertext
 
 
@@ -26,10 +37,10 @@ def xor(a, b):
     return bytes(result)
 
 
-code = b"alert('MZA who was that?');\n\4\4\4\4"
+code = b"alert('MZA who was that?');\n"
 key = b"YELLOW SUBMARINE"
 
-assert mac(key, b'hello world\5\5\5\5\5') == ecb(key, b'hello world\5\5\5\5\5')
+assert mac(key, b'hello world') == ecb(key, b'hello world\5\5\5\5\5')
 
 hash = mac(key, code)
 assert hexlify(hash) == b"296b8d7cb78a243dda4d0a61d33bbdd1"
@@ -40,10 +51,14 @@ partial = b"alert('MZA who w"
 partial_mac = ecb(key, partial)
 xor_result = xor(partial_mac, b"as that?');\n\4\4\4\4")
 
+assert xor_result == ecb_decrypt(key, hash)
+
+previous_xor_result = ecb_decrypt(key, xor(xor_result, bytes([16]) * 16))
+
 
 new_code = b"alert('Ayo, the Wu is back!');               // "
-new_mac = mac(key, new_code)
-data_for_same_xor = xor(new_mac, xor_result)
+new_mac = mac(key, new_code, False)
+data_for_same_xor = xor(new_mac, previous_xor_result)
 
 new_code += data_for_same_xor
 assert len(new_code) % 16 == 0
