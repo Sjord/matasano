080b89238ec863bb0d224ef7963ef0c9a68a1011
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2016-04-04 10:37:28 +0200

    CBC-MAC Message Forgery

--- set7/forgecbc.py
+++ set7/forgecbc.py
@@ -1,16 +1,15 @@
 from Crypto.Cipher import AES
 import os
 
-def mac(key, plaintext, iv=None):
+def mac(key, plaintext):
     while len(plaintext) % 16 != 0:
         plaintext += b'\0'
 
-    if iv is None:
-        iv = os.urandom(16)
+    iv = b'\0' * 16 
     aes = AES.new(key, AES.MODE_CBC, IV=iv)
     ciphertext = aes.encrypt(plaintext)
     last_block = ciphertext[-16:]
-    return (last_block, iv)
+    return last_block
     
     
 class Client:
@@ -22,46 +21,44 @@ class Client:
         message = b"from=%s&to=%s&amount=%s" % (self._account, to, amount)
         return message, mac(self._key, message)
 
+    def create_transfers(self, transfers):
+        transfer_bytes = []
+        for to, amount in transfers:
+            transfer_bytes.append(b"%s:%s" % (to, amount))
+            
+        message = b"from=%s&tx_list=%s" % (self._account, b":".join(transfer_bytes))
+        return message, mac(self._key, message)
+
 
 class Server:
     def __init__(self, key):
         self._key = key
 
     def verify_transfer(self, transfer):
-        message, (last_block, iv) = transfer
-        good_mac = mac(self._key, message, iv)
-        return good_mac[0] == last_block
+        message, last_block = transfer
+        good_mac = mac(self._key, message)
+        return good_mac == last_block
 
 client = Client(b'some random key.', b'1234567')
 server = Server(b'some random key.')
-transfer = client.create_transfer(b'1000000', b'1234567')
+transfer = client.create_transfers([(b'2345678', b'100')])
 
-(message, (last_block, iv)) = transfer
-# message = b'from=1234567&to=2345678&amount=123'
+(message, last_block) = transfer
 
-message = bytearray(message)
-# 1234567 -> 2345678
-message[5] ^= 3
-message[6] ^= 1
-message[7] ^= 7
-message[8] ^= 1
-message[9] ^= 3
-message[10] ^= 1
-message[11] ^= 15
-message = bytes(message)
-
-iv = bytearray(iv)
-iv[5] ^= 3
-iv[6] ^= 1
-iv[7] ^= 7
-iv[8] ^= 1
-iv[9] ^= 3
-iv[10] ^= 1
-iv[11] ^= 15
-iv = bytes(iv)
-
+extension, extension_mac = client.create_transfers([(b'3456789', b'1000000')])
 print(message)
+print(extension)
 
-res = server.verify_transfer((message, (last_block, iv)))
+# xor last_block with desired plaintext ourselves and let client sign it
+# client only signs from=1234567tx_l
+# so let last_block ^ plaintext == from=1234567tx_l
+
+extension = bytearray(extension)
+for i in range(16):
+    extension[i] ^= last_block[i]
+extension = bytes(extension)
+
+extended_message = message + extension
+print(extended_message)
+res = server.verify_transfer((extended_message, extension_mac))
 print(res)
-
